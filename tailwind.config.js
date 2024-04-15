6043e1e4e2a86e0103a2be0da5862d442a034e9e
Furkan Soylu <fsoylu567@gmail.com>
2024-04-15 18:16:08 +0300

    Customizing Fonts, Colors, Container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        garamond: ['EB Garamond', 'serif'],
+        allura: ['Allura', 'cursive'],
+      },
+
+      colors: {
+        'main-color-one'  : '#05445e',
+        'main-color-two'  : '#02fced',
+        'animate'         : '#195c78',
+        'buble'           : '#075f83',
+      }
+    },
   },
   plugins: [],
 }
